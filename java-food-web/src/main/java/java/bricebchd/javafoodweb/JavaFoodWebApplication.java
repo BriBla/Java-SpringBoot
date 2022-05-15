9d2826b61d2eb3cd652edb1d7530123b2641419a
Brice Blanchard <brice.blanchard@hotmail.fr>
2022-05-15 23:23:49 +0200

    first project model*

---
+++ java-food-web/src/main/java/java/bricebchd/javafoodweb/JavaFoodWebApplication.java
@@ -0,0 +1,13 @@
+package java.bricebchd.javafoodweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFoodWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFoodWebApplication.class, args);
+	}
+
+}
---
+++ java-food-web/src/test/java/java/bricebchd/javafoodweb/JavaFoodWebApplicationTests.java
@@ -0,0 +1,13 @@
+package java.bricebchd.javafoodweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFoodWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
