cd0879f39a349e2aea461f8298f5f49e37308c11
Brice Blanchard <brice.blanchard@hotmail.fr>
2022-05-22 22:31:01 +0200

    add updater

--- java-spring-boot/src/main/java/test/brice/CRUD/CrudController.java
+++ java-spring-boot/src/main/java/test/brice/CRUD/CrudController.java
@@ -1,21 +1,27 @@
 package test.brice.CRUD;
 
+import java.lang.annotation.Target;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Controller
-@RequestMapping(path="/user")
+@RequestMapping(path = "/user")
 public class CrudController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping(path="/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
+    @PostMapping(path = "/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
         User n = new User();
         n.setName(name);
         n.setEmail(email);
@@ -23,9 +29,19 @@ public class CrudController {
         return "Saved";
     }
 
-    @GetMapping(path="/all")
+    @GetMapping(path = "/all")
     public @ResponseBody Iterable<User> getAllUsers() {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
+    }
+
+    @PutMapping(path = "/update/{id}")
+    public @ResponseBody ResponseEntity<User> updateUser(@RequestParam String name,
+            @RequestParam String email, @PathVariable Integer id) {
+        User userFromDb = userRepository.findById(id).get();
+        userFromDb.setName(name);
+        userFromDb.setEmail(email);
+        userRepository.save(userFromDb);
+        return new ResponseEntity<>(userFromDb, HttpStatus.OK);
     }
 }
