413e644e5960d99e8ec5a0de3072bb6822a01a85
Brice Blanchard <brice.blanchard@hotmail.fr>
2022-05-22 22:43:56 +0200

    add delete

--- java-spring-boot/src/main/java/test/brice/CRUD/CrudController.java
+++ java-spring-boot/src/main/java/test/brice/CRUD/CrudController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,11 +38,17 @@ public class CrudController {
 
     @PutMapping(path = "/update/{id}")
     public @ResponseBody ResponseEntity<User> updateUser(@RequestParam String name,
-            @RequestParam String email, @PathVariable Integer id) {
+            @RequestParam String email, @PathVariable("id") Integer id) {
         User userFromDb = userRepository.findById(id).get();
         userFromDb.setName(name);
         userFromDb.setEmail(email);
         userRepository.save(userFromDb);
         return new ResponseEntity<>(userFromDb, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") Integer id) {
+        userRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
